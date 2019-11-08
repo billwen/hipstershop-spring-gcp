7fc513386676eb3529f210702c6e258e1d86dd47
Bill Wen <gang.wen@gmail.com>
2019-11-08 22:51:43 +0100

    Added generated frontend project skeleton
    generate project files from https://start.spring.io with 14 starters.

---
+++ hipster-shop-frontend/src/main/java/com/billwen/hipstershopfrontend/HipsterShopFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.billwen.hipstershopfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HipsterShopFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HipsterShopFrontendApplication.class, args);
+	}
+
+}
---
+++ hipster-shop-frontend/src/test/java/com/billwen/hipstershopfrontend/HipsterShopFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billwen.hipstershopfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HipsterShopFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
